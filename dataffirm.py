ceacda14eb69110076f1c6e604d0c9569f559c50
gpsyrou <gspyrou@umich.edu>
2018-06-11 12:23:07 +0300

    new

--- dataffirm.py
+++ dataffirm.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.tools.plotting import lag_plot,autocorrelation_plot
+from statsmodels.tsa.stattools import adfuller
 from datetime import datetime
 
 url = 'https://raw.githubusercontent.com/gpsyrou/Apple_Analysis/master/Apple_Revenues.csv'
@@ -59,6 +60,7 @@ plt.show()
 # case of 2006 where the revenue per quarter ranged between 4.3 to 7.1 billion , while ten years later
 # at 2016 for the same periods(quarters) it ranged from 50.5 to 78.3 billions
 
+# Its clear that there is an overall increasing trend in the data along with some seasonal variations
 
 # Time series Lag scatter plot
 plt.figure(figsize=(5,5))
@@ -111,6 +113,123 @@ plt.tight_layout()
 plt.show()
 
 
+#### Time series forecast
+
+# We load the dataset again, but this time we hold Date as the Index as it will be useful
+# for the time series forecast
+
+dt_parse = lambda dates: pd.datetime.strptime(dates,'%d/%m/%Y')
+data_ts = pd.read_csv(url,parse_dates = ['Date'], index_col = 'Date' , date_parser = dt_parse)
+print(data_ts.head())
+
+# Again, delete the observations for 2005 and 2018 as they are insufficient
+
+data_ts = data_ts.drop(data_ts.index[-1])
+data_ts = data_ts.drop(data_ts.index[0])
+print(data_ts.head())
+
+
+# Stationarity : We already observed before that our data appear (plot of the ts) to have a stationary form
+# which is one of the required assumptions for Time Series
+
+# Now we can check for the constant mean,constant variance requirements of stationarity
+
+
+def stationarity_checking(ts,win):
+    
+    # Rolling mean,std:
+    rolling_mn = pd.rolling_mean(ts, window = win )
+    rolling_std = pd.rolling_std(ts, window  = win)
+    
+    plt.plot(ts, color='blue',label='Original TS')
+    plt.plot(rolling_mn, color='red', label='Rolling Mean')
+    plt.plot(rolling_std, color='black', label = 'Rolling St.Dev.')
+    plt.legend(loc='best')
+    plt.grid(True, color = 'lightgrey')
+    plt.title('Rolling Mean & Standard Deviation of Revenues')
+    
+    # Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    fuller_test = adfuller(ts['Revenue(Quarterly)'], autolag='AIC')
+    results_ts = pd.Series(fuller_test[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in fuller_test[4].items():
+        results_ts['Critical Value (%s)'%key] = value
+    print(results_ts)
+
+stationarity_checking(data_ts,3) # we use 3 as we take into consideration the t-1,t-2,t-3 values ( average of last 3 values)
+
+
+# Thus we can observe that we have  high variation for the standard deviation. For the mean we can see that its increasing in general,
+# although its clear that there are some up and downs .
+# From the Dickey-Fuller  test we can see that the test statistic is more than the critical values.
+# Therefore as we expected , the time series model is not stationary yet.
+# Our issue behind non-stationarity is the fact that we have a constant increase in the Revenues,something that has an impact at the mean
+# as well as, some seasonality issues-which we can observe at the plot.
+
+# Reducing Trend (fluctuations of the mean):
+
+
+
+# Log-transformation  in order to penalize the extreme values
+
+data_log = np.log(data_ts)
+
+moving_average_new = pd.rolling_mean(data_log ,3)
+
+plt.plot(data_log, color = 'orange' , label = 'Original TS')
+plt.plot(moving_average_new,color = 'red', label = 'Rolling Mean')
+plt.title("Moving Average - Original Revenues Plot", fontsize = 13)
+plt.ylabel("Quarterly Log-Revenue in Billions($)")
+plt.legend(loc='best')
+plt.grid(True, color = 'lightgrey')
+
+# Method 1 - Then we subtract the rolling mean from the original values in the initial series
+
+data_moving_avg_diff = data_log - moving_average_new
+print(data_moving_avg_diff.head())
+
+# first two are NaN as we test for average of last 3 values, and thus we can drop them and test for stationarity
+data_moving_avg_diff.dropna(inplace = True)
+stationarity_checking(data_moving_avg_diff,3)
+
+# We can see that this looks like a much better series than before! The Test statistic is closer to the critical values
+
+# Method 2 - Exponentially Weighted Moving Average Technique(ewma)
+# where we assign weights to all the previous values with a decay factor
+
+ewma_avg = pd.ewma(data_log, halflife = 3)
+data_ewma_diff = data_log - ewma_avg
+stationarity_checking(data_ewma_diff,3)
+
+# Method 3 - Differencing
+
+data_log_diff = data_log - data_log.shift()
+plt.plot(data_log_diff)
+data_log_diff.dropna(inplace = True)
+stationarity_checking(data_log_diff,3)
+
+# The results are getting better as we approaching a -somehow- stationary series.
+
+
+# Method 4 - Decomposition
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+decomposition = seasonal_decompose(data_log)
+
+trend = decomposition.trend
+seasonal = decomposition.seasonal
+residual = decomposition.resid
+
+data_log_decomp = residual
+data_log_decomp.dropna(inplace = True)
+stationarity_checking(data_log_decomp, 3)
+
+# Thats our best model ! We can clearly observe that our Test-Statistic is lower than the 1% critical value and therefore
+# this is very close to stationary!
+
+
+# Prediction 
+# For the prediction purposes we will use the 3'rd method from above i.e. the Differencing
 
 
 
